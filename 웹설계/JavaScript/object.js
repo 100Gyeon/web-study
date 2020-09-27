40726b0f9865dccd381b3066d9f2feed5fcce367
100Gyeon <bjy9966@naver.com>
2020-09-27 21:41:32 +0900

    JavaScript 객체

---
+++ 웹설계/JavaScript/object.js
@@ -0,0 +1,17 @@
+// 객체는 중괄호 사용해서 선언
+let product = {
+    // key: value
+    제품명: '7D 건조 망고',
+    유형: '당절임',
+    성분: '망고, 설탕, 메타중아황산나트륨, 치자황색소',
+    원산지: '필리핀'
+};
+
+let object = {
+    name: '바나나',
+    price: 1200
+}
+
+console.log(product);
+console.log(object.name);
+console.log(object['price']);
---
+++ 웹설계/JavaScript/objectLoop.js
@@ -0,0 +1,20 @@
+// 객체 선언
+let object = {
+    name: '바나나',
+    price: 1200
+};
+
+// 배열은 순서가 있는 iterable (for ... of)
+// 객체는 순서가 없는 enumerable (for ... in)
+// for in 반복문을 통해 객체에 반복문을 적용
+for(let key in object){
+    console.log("key : " + object[key]);
+}
+
+let obj = {
+    'name': 'apple',
+    'price': 1500
+}
+for(let key in obj) {
+    console.log(key + " : " + obj[key]);
+}
