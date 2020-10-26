f8d4a77201d9cff7d73f83af78e48a9a8669d47d
100Gyeon <bjy9966@naver.com>
2020-10-27 03:24:19 +0900

    require의 특성

--- 웹설계/Node.js/require.js
+++ 웹설계/Node.js/require.js
@@ -1,11 +1,12 @@
 console.log('require가 가장 위에 오지 않아도 됩니다.');
+// 필요한 부분에서 require(파일 경로) 해서 쓰면 된다.
 
 module.exports = '저를 찾아보세요.';
 
 require('./var');
 
 console.log('require.cache입니다.');
-console.log(require.cache);
+console.log(require.cache); // 처음으로 require한 모듈에 대한 캐싱 정보 들어있음
 console.log('require.main입니다.')
-console.log(require.main === module);
+console.log(require.main === module); // 노드 실행 시 첫 모듈을 가리킴
 console.log(require.main.filename);
