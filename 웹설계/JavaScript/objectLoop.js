2fcdc96e67f9d89e179fddd5bff59f04a6c5066e
100Gyeon <bjy9966@naver.com>
2020-10-26 18:06:49 +0900

    객체와 반복문

--- 웹설계/JavaScript/objectLoop.js
+++ 웹설계/JavaScript/objectLoop.js
@@ -8,7 +8,7 @@ let object = {
 // 객체는 순서가 없는 enumerable (for ... in)
 // for in 반복문을 통해 객체에 반복문을 적용
 for(let key in object){
-    console.log("key : " + object[key]);
+    console.log(`${key}: ${object[key]}`);
 }
 
 let obj = {
