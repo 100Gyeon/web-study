92e8748118c21d1e77e67041b135a0665879d366
100Gyeon <bjy9966@naver.com>
2020-10-26 17:51:14 +0900

    익명 함수와 선언적 함수의 생성 순서

--- 웹설계/JavaScript/functionOverwrite.js
+++ 웹설계/JavaScript/functionOverwrite.js
@@ -1,16 +1,17 @@
-/*
-* 호이스팅(Hoisting) : Global 영역에 선언된 코드를 최상의 scope로 끌어올림
-*/
+// 호이스팅(Hoisting) : Global 영역에 선언된 코드를 최상의 scope로 끌어올림
 
 // 익명 함수가 선언적 함수를 덮어씀
 a = function () { console.log("1") };
 function a() { console.log("2") }; // 선언적 함수가 익명 함수보다 먼저 생성된다.
 a(); // 1
 
-// 이 경우는 오류 발생 (let을 붙이면 block scope으로 처리된다.)
-// let b = function () { console.log("1번 함수") };
-// function b() { console.log("2번 함수") };
-// b();
+/* 이 경우는 오류 발생 (let을 붙이면 block scope으로 처리된다.)
+
+let b = function () { console.log("1번 함수") };
+function b() { console.log("2번 함수") };
+b();
+
+*/
 
 c = function () { console.log("1"); };
 c = function () { console.log("2"); };
