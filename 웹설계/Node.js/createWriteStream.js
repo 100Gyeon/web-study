757bf477c457efe4d0a5d0ca44b80da1c5dcded7
100Gyeon <bjy9966@naver.com>
2020-10-27 05:17:21 +0900

    파일 쓰는 스트림 사용

--- 웹설계/Node.js/createWriteStream.js
+++ 웹설계/Node.js/createWriteStream.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 
 const writeStream = fs.createWriteStream('./write2.txt');
+
+// 이벤트 리스너
 writeStream.on('finish', () => {
     console.log('파일 쓰기 완료');
 });
 
+// 실제 write는 여기서 이루어진다. write로 chunk 입력
 writeStream.write('이 글을 씁니다.\n');
 writeStream.write('한 번 더 씁니다.');
+
+// end로 스트림 종료
 writeStream.end();
