fbd49e6efc85e2ffa1d260f60baa200828820ec0
100Gyeon <bjy9966@naver.com>
2020-10-26 17:14:39 +0900

    setTimeout, setInterval, clearInterval (타이머 함수)

--- 웹설계/JavaScript/timer.js
+++ 웹설계/JavaScript/timer.js
@@ -1,16 +1,17 @@
-// 1초 후에 함수 실행
 // setTimeOut(함수, 시간)
+// 아래 예제는 1초 후에 함수 실행
 setTimeout(function () {
     console.log("1초가 지났습니다.");
 }, 1000);
 
-// 1초마다 함수 실행 (종료는 Ctrl+C)
 // setInterval(함수, 시간)
-id = setInterval( function () {
+// 아래 예제는 1초마다 함수 실행 (종료는 Ctrl+C)
+let id = setInterval( function () {
     console.log("1초 마다 호출됩니다.");
 }, 1000);
 
 // clearInterval(아이디)
+// 특정 시간마다 실행하던 함수 호출 정지
 setTimeout(function () {
-    clearInterval(id); // 3초 후에 타이머 제거
-}, 3000);
+    clearInterval(id);
+}, 3000); // 3초 후에 타이머 제거
