ac3ab96c92786dd68646918d44a9aa8299798255
100Gyeon <bjy9966@naver.com>
2020-10-26 17:09:16 +0900

    parseInt(), parseFloat()

---
+++ 웹설계/JavaScript/parseNumber.js
@@ -0,0 +1,8 @@
+let inputA = "52"
+let inputB = "52.273"
+let inputC = "1401동"
+
+console.log(parseInt(inputA)); // 52
+console.log(parseInt(inputB)); // 52
+console.log(parseFloat(inputB)); // 52.273
+console.log(parseInt(inputC)); // 1401 (해석할 수 있는 부분만 처리)
