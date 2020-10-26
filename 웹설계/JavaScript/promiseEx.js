6da26ea3b99d06d9c873e8b00aecdb96599fa566
100Gyeon <bjy9966@naver.com>
2020-10-27 00:55:50 +0900

    promise, async/await, 화살표 함수

--- 웹설계/JavaScript/promiseEx.js
+++ 웹설계/JavaScript/promiseEx.js
@@ -14,3 +14,30 @@ function findAndSaveUser(Users) {
         console.error(err);
     });
 }
+
+// 프로미스 패턴 코드를 async/await으로 축약 가능
+// 에러 처리를 위해 try catch로 감싸줌
+async function findAndSaveUser(Users) {
+    try {
+        let user = await Users.findOne({});
+        user.name = 'zero';
+        user = await user.save();
+        user = await Users.findOne({gender: 'm'});
+        // 생략
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// 화살표 함수도 async/await 가능
+const findAndSaveUser = async (Users) => {
+    try {
+        let user = await Users.findOne({});
+        user.name = 'zero';
+        user = await user.save();
+        user = await Users.findOne({gender: 'm'});
+        // 생략
+    } catch (error) {
+        console.error(error);
+    }
+}
