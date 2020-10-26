f6b6dd2b0efcef02722fe80e50de4a8283f85ec3
100Gyeon <bjy9966@naver.com>
2020-10-27 03:24:51 +0900

    module.exports와 exports

--- 웹설계/Node.js/var.js
+++ 웹설계/Node.js/var.js
@@ -1,3 +1,4 @@
+// module.exports를 통해 모듈로 만들 값 지정
 const odd = '홀수입니다';
 const even = '짝수입니다';
 
@@ -5,4 +6,10 @@ module.exports = {
     odd,
     even,
 };
-// module.exports를 통해 모듈로 만들 값 지정
+
+// module.exports 외에도 exports로 모듈 만들 수 있음
+// 위의 코드를 아래 코드로 바꿔도 동일하게 동작함
+/*
+exports.odd = '홀수입니다';
+exports.even = '짝수입니다';
+*/
