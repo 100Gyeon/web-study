5f415cc3fda58b74b60142d23534cfa5a3651e30
100Gyeon <bjy9966@naver.com>
2021-01-01 19:25:57 +0900

    event, state, props

--- react/src/App.js
+++ react/src/App.js
@@ -5,14 +5,39 @@ import Subject from "./components/Subject"
 import './App.css';
 
 // component를 만드는 코드
+// props, state 값이 바뀌면 해당 컴포넌트의 render 함수가 호출된다. (= 화면이 다시 그려진다.)
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode:'welcome',
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc: 'Hello, React!!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+      ]
+    }
+  }
   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode ==='read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return ( 
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
