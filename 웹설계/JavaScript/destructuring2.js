1b1f448543c8e3426c1909f00b2d708be4cef0f8
100Gyeon <bjy9966@naver.com>
2021-07-30 20:05:52 +0900

    배열 구조분해 할당

--- 웹설계/JavaScript/destructuring2.js
+++ 웹설계/JavaScript/destructuring2.js
@@ -7,3 +7,13 @@ var bool = array[3];
 const array = ['nodejs', {}, 10, true];
 const [node, obj, , bool] = array; // 3번째가 비어있는 것은 버리는 값을 의미
 // const [변수] = 배열;
+
+const arr = [1, 2];
+const [one, two] = array;
+console.log(one); // 1
+console.log(two); // 2
+
+const arr = [1];
+const [one, two = 2] = array; // two의 기본 값을 2로 설정
+console.log(one); // 1
+console.log(two); // 2
