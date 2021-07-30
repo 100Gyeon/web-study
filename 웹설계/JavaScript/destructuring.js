5700dffaaa3ad6719fc6cf6c99de80bcc8547d57
100Gyeon <bjy9966@naver.com>
2021-07-30 20:17:13 +0900

    객체 구조분해 할당

--- 웹설계/JavaScript/destructuring.js
+++ 웹설계/JavaScript/destructuring.js
@@ -19,3 +19,25 @@ const { getCandy, status: { count } } = candyMachine;
 
 console.log(getCandy);
 console.log(count);
+
+// 다른 예제
+const deepObject = {
+    state: {
+        information: {
+            name: 'Mark',
+            languages: ['korean', 'english', 'chinese']
+        }
+    },
+    value: 5
+};
+
+const { name, languages } = deepObject.state.information;
+const { value } = deepObject;
+
+const extracted = {
+    // 특정 객체를 만들 때, 특정 키 이름으로 선언된 값이 이미 있다면 value 값 설정을 생략해도 된다.
+    name, // name: name에서 value를 생략한 형태
+    languages,
+    value
+};
+console.log(extracted); // {name: "Mark", languages: Array(3), value: 5}
