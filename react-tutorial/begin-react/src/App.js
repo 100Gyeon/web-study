d3cb12c1a13b33213c14ecb19dfd3ee5b31f4df8
100Gyeon <bjy9966@naver.com>
2021-08-03 22:20:03 +0900

    props를 통해 컴포넌트에게 값 전달하기

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import Hello from './Hello'; // 자신의 디렉터리에 있는 Hello라는 컴포넌트를 불러오겠다. (상대경로)
-import './App.css';
+import Wrapper from './Wrapper';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
-  }
   return (
-    <>
-      {/* 주석 작성 방법 */}
-      <Hello
-        // 여기도 주석 작성 가능
-      />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    <Wrapper>
+      <Hello name="react" color="red" />
+      <Hello color="pink" />
+    </Wrapper>
   );
 }
 
--- react-tutorial/begin-react/src/Hello.js
+++ react-tutorial/begin-react/src/Hello.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 // 클래스, 함수 형태로 컴포넌트 작성
 // 컴포넌트 이름은 대문자로 시작
-function Hello() {
-  return <div>안녕하세요!</div>;
+function Hello({ color, name }) {
+  return <div style={{
+    color
+  }}>안녕하세요 {name}</div>;
 }
 
+Hello.defaultProps = {
+  name: '이름없음'
+}
 // Hello라는 컴포넌트를 만들어서 내보냄
 export default Hello;
