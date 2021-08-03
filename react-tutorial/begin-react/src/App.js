160b3aa68647a57da6ab5f5be8ae4d71b8740ce1
100Gyeon <bjy9966@naver.com>
2021-08-03 22:05:47 +0900

    JSX

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Hello from './Hello'; // 자신의 디렉터리에 있는 Hello라는 컴포넌트를 불러오겠다. (상대경로)
+import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
   return (
-    <div>
-      <Hello />
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+    <>
+      {/* 주석 작성 방법 */}
+      <Hello
+        // 여기도 주석 작성 가능
+      />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
