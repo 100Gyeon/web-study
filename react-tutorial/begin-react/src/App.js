1274a31eb603f2ced302e49385dddd77e4bf6b52
100Gyeon <bjy9966@naver.com>
2021-08-04 14:53:28 +0900

    useState를 통해 컴포넌트에서 바뀌는 값 관리하기

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Hello from './Hello'; // 자신의 디렉터리에 있는 Hello라는 컴포넌트를 불러오겠다. (상대경로)
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial={true} />
-      <Hello color="pink" />
-    </Wrapper>
+    <Counter />
   );
 }
 
---
+++ react-tutorial/begin-react/src/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  // useState를 통해서 바뀌는 값을 관리할 수 있음
+  // const [현재 상태, 상태를 바꾸는 함수] = useState(기본값);
+  // useState를 사용할 때는 상태의 기본값을 파라미터로 넣어서 호출
+
+  const [number, setNumber] = useState(0);
+  const onIncrease = () => {
+    setNumber(prevNumber => prevNumber + 1);
+  }
+  const onDecrease = () => {
+    setNumber(prevNumber => prevNumber - 1);
+  }
+  return (
+    <div>
+      <h1>{number}</h1>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+    </div>
+  )
+}
+
+export default Counter;
