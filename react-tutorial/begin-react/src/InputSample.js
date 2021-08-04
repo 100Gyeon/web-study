278ce79805fbe5835ecc04aea71e5bfc898ac5e6
100Gyeon <bjy9966@naver.com>
2021-08-04 17:57:08 +0900

    여러 개의 input 상태 관리하기

--- react-tutorial/begin-react/src/InputSample.js
+++ react-tutorial/begin-react/src/InputSample.js
@@ -1,20 +1,36 @@
 import React, { useState } from 'react';
 
 function InputSample() {
-  const [text, setText] = useState('');
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: '',
+  });
+  const { name, nickname } = inputs;
   const onChange = (e) => {
-    setText(e.target.value);
-  }
+    // console.log(e.target.name);
+    // console.log(e.target.value);
+    const { name, value } = e.target;
+    // 객체 상태를 업데이트 할 때는, spread 문법 사용해서 객체 복사하고 난 뒤
+    // 특정 값을 덮어씌워서 업데이트
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value, // name 키를 가진 값을 value로 설정
+    });
+  };
   const onReset = () => {
-    setText('');
-  }
+    setInputs({
+      name: '',
+      nickname: '',
+    });
+  };
   return (
     <div>
-      <input onChange={onChange} value={text} />
+      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
       <button onClick={onReset}>초기화</button>
       <div>
         <b>값 : </b>
-        {text}
+        {name} ({nickname})
       </div>
     </div>
   )
