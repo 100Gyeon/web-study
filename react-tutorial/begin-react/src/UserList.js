be1ef8e2c4d752990da43a78d6884d2f1294914b
100Gyeon <bjy9966@naver.com>
2021-08-19 11:15:35 +0900

    배열 렌더링하기

--- react-tutorial/begin-react/src/UserList.js
+++ react-tutorial/begin-react/src/UserList.js
@@ -1,3 +1,5 @@
+/* 배열 렌더링하기 */
+
 import React from 'react';
 
 function User({ user }) {
@@ -14,9 +16,11 @@ function UserList({ users }) {
       {
         users.map(
           user => (<User user={user} key={user.id} />)
+          // (user, index) => (<User user={user} key={index} />)
         )
         // 각 요소들마다 고유한 값(key)을 가지고 있어야 함
-        // 새로운 항목이 추가/제거될 때 효율적으로 업데이트
+        // id처럼 고유한 값이 없으면 key에 index를 넣어줄 수는 있지만 성능적으로 좋아지진 않음
+        // key가 있어야 새로운 항목이 추가/제거될 때 효율적으로 업데이트
       }
     </div>
   );
