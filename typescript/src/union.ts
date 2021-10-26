3df116b3281bea5b960b5d892f9441c1b8ebaa1a
100Gyeon <bjy9966@naver.com>
2021-10-27 07:22:49 +0900

    type assertion, type guard

--- typescript/src/union.ts
+++ typescript/src/union.ts
@@ -48,14 +48,18 @@ function introduce2(): Developer | Person {
   return { name: "Jiyeon", age: 23, skill: "typescript" };
 }
 const jiyeon = introduce2();
-// console.log(jiyeon.name) -> 접근 가능
-// console.log(jiyeon.skill); -> 오류 생김
-// union type에서는 공통된 속성에만 접근할 수 있기 때문
+/*
+  console.log(jiyeon.name) -> 접근 가능
+  console.log(jiyeon.skill); -> 오류 생김
+  union type에서는 공통된 속성에만 접근할 수 있기 때문
+*/
 
-// Type Assertion (타입 단언)
-// 타입 단언 문법은 <Type>과 as Type으로 두 종류다. (후자 추천)
-// 타입 추론을 무시하고 개발자가 직접 타입을 지정할 수 있음
-// Developer와 Person으로 각각 지정한 뒤, 콘솔에 출력하는 방식
+/*
+  Type Assertion (타입 단언)
+  타입 단언 문법은 <Type>과 as Type으로 두 종류다. (후자 추천)
+  타입 추론을 무시하고 개발자가 직접 타입을 지정할 수 있음
+  Developer와 Person으로 각각 지정한 뒤, 콘솔에 출력하는 방식
+*/
 if ((jiyeon as Developer).skill) {
   const skill = (jiyeon as Developer).skill;
   console.log(skill);
@@ -63,16 +67,16 @@ if ((jiyeon as Developer).skill) {
   const age = (jiyeon as Person).age;
   console.log(age);
 }
-// 타입을 단언해서 skill과 age를 사용할 수 있게 되지만
-// 가독성, 효율성 좋지 않음
+// 타입을 단언해서 skill과 age를 사용할 수 있게 되지만 가독성, 효율성 좋지 않음
 
-// Type Guard (is 키워드 사용)
-// 특정 타입인지 검사하는 함수를 정의
-// is + 타입명을 사용하는 것이 관행 (ex. isDeveloper)
-// 실제 타입 검사를 하는 메소드의 리턴 타입으로 {variable} is {Type} 같은 문법을 사용해 선언
+/*
+  Type Guard (is 키워드 사용)
+  특정 타입인지 검사하는 함수를 정의
+  is + 타입명을 사용하는 것이 관행 (ex. isDeveloper)
+  실제 타입 검사를 하는 메소드의 리턴 타입으로 {variable} is {Type} 같은 문법을 사용해 선언
+*/
 function isDeveloper(target: Developer | Person): target is Developer {
-  // Developer.skill이 undefined가 아니면
-  // 즉 Developer.skill이 있다면 Developer 타입이 된다.
+  // Developer.skill이 undefined가 아니면 (즉 Developer.skill이 있다면) Developer 타입이 된다.
   return (target as Developer).skill !== undefined;
 }
 // Type Guard 덕분에 if문이 더 간단해짐
